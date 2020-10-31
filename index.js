4fdc22d5dfecd73d67fd31ed6d30ca58241f0e21
fanlan1210 <fanlan1210@users.noreply.github.com>
2020-11-01 01:34:31 +0800

    Full update index

---
+++ index.js
@@ -0,0 +1,5 @@
+ScrollReveal({ duration: 1000 });
+ScrollReveal().reveal('#stu');
+ScrollReveal().reveal('#group', { delay: 1000 });
+ScrollReveal().reveal('#skill', { delay: 2000 });
+ScrollReveal().reveal('#more', { delay: 3000 });
